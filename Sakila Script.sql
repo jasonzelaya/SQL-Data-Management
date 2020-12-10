24ea742a2a3252d1a224b9e9aa780482baf98450
Jason Zelaya <Jason.Zelaya474@gmail.com>
2020-12-09 19:50:20 -0800

    Complete part of section 7 in sakila
    Write code for 7a-7d for the sakila database instructions.

--- Sakila Script.sql
+++ Sakila Script.sql
@@ -148,13 +148,68 @@ SELECT c.customer_id,
  ORDER BY c.last_name;
 
 
--- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+/* Display the titles of movies starting with the letters K and Q whose language is English (7a) */
+SELECT film_id,
+	   title
+  FROM film
+ /* Filter out records that do not contain "K" or "Q" in their film titles and whose language is not English */
+ WHERE (title LIKE 'K%'
+    OR title LIKE 'Q%')
+   AND language_id = 
+	   /* Return the language ID for English */
+	   (SELECT language_id
+          FROM language
+		 /* Filter out all language IDs that are not associated with English */
+		 WHERE name = 'English');
 
--- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
 
--- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+/* Display all actors who appear in the film Alone Trip (7b) */
+SELECT actor_id,
+	   first_name, last_name
+  FROM actor
+ /* Filter out all actors who do not appear in "Alone Trip" */
+ WHERE actor_id IN
+	   /* Return all actor IDs associated with "Alone Trip" */
+	   (SELECT actor_id
+          FROM film_actor
+		 /* Filter out all film IDs not associated with "Alone Trip" */
+		 WHERE film_id =
+			   /* Return the film ID for "Alone Trip" */
+			   (SELECT film_id
+                  FROM film
+				 /* Filter out all films except "Alone Trip" */
+				 WHERE title = 'Alone Trip'
+				)
+		) ;
+        
 
--- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
+/* Display the names and email addresses of all Canadian customers (7c) */
+SELECT c1.customer_id, 
+	   c1.first_name, c1.last_name, c1.email, c3.country
+  FROM customer AS c1
+	   /* Combine the tables based on the matching values of their related columns */
+	   INNER JOIN address AS a
+       ON c1.address_id = a.address_id
+       INNER JOIN city AS c2
+       ON a.city_id = c2.city_id
+       INNER JOIN country AS c3
+       ON c2.country_id = c3.country_id
+ /* Filter out all records not associated with Canadians */
+ WHERE c3.country = 'Canada';
+       
+
+/* Display all movies categorized as family films (7d) */
+SELECT f1.film_id,
+	   f1.title, c.name AS film_category
+  FROM film AS f1
+	   /* Combine all tables based on matching values in their related columns */
+	   INNER JOIN film_category AS f2
+       ON f1.film_id = f2.film_id
+       INNER JOIN category AS c
+       ON f2.category_id = c.category_id
+ /* Filter out all non-family films */
+ WHERE name = 'Family';
+
 
 -- 7e. Display the most frequently rented movies in descending order.
 
